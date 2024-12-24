824c11e6985a2323da0fb0e0461e584c0fc24c61
giochac <giodevito99@gmail.com>
2024-12-24 08:16:21 +0100

    Design of the page

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,34 @@
-console.log('Happy developing ✨... Updating live!!!')
+console.log('active script')
+const pagesReadInput = document.querySelector('#pagesRead') as HTMLElement;
+const addBookCard = document.querySelector('.add-book-card') as HTMLElement;
+const addBookPage = document.querySelector('.add-book-page') as HTMLElement;
+const removeBookBtn = document.querySelector('.remove-book-btn') as HTMLElement;
+const body = document.querySelector('body') as HTMLElement;
+
+const read = document.querySelector('input[type="checkbox"]');
+
+read?.addEventListener('click', () => {
+    toggleHide(pagesReadInput);
+})
+
+function toggleScroll() {
+    if (body.classList.contains('no-scroll'))
+        body.classList.remove('no-scroll');
+    else
+        body.classList.add('no-scroll')
+}
+
+function toggleHide(element: HTMLElement) {
+    if (element?.classList.contains('hidden'))
+        element?.classList.remove('hidden')
+    else
+        element?.classList.add('hidden')
+}
+addBookCard?.addEventListener('click', () => {
+    toggleScroll()
+    toggleHide(addBookPage);
+})
+removeBookBtn?.addEventListener('click', () => {
+    toggleScroll()
+    toggleHide(addBookPage);
+})
